f401b8f676d03397e3ab50c94e1ddcdf747a5c12
Vyacheslav Kan <vkan745@gmail.com>
2024-09-05 16:31:31 +0500

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210 #511996] (откорректировано наименование константы)

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -17,10 +17,10 @@ public class HabrCareerParse implements DateTimeParser {
     private static final String SOURCE_LINK = "https://career.habr.com";
     public static final String PREFIX = "/vacancies?page=";
     public static final String SUFFIX = "&q=Java%20developer&type=all";
-    private static final int pageCount = 5;
+    private static final int PAGE_COUNT = 5;
 
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i <= pageCount; i++) {
+        for (int i = 1; i <= PAGE_COUNT; i++) {
             int pageNumber = i;
             String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
             Connection connection = Jsoup.connect(fullLink);
