af16afa7722903f292e91a8ba85e7a3ed1a6dde6
Vyacheslav Kan <vkan745@gmail.com>
2024-09-09 15:44:49 +0500

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,86 @@
+package ru.job4j.grabber;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store {
+
+    private Connection connection;
+
+    public PsqlStore(Properties config) throws SQLException {
+        try {
+            Class.forName(config.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        connection = DriverManager.getConnection(config.getProperty("url"), config.getProperty("user"), config.getProperty("password"));
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement statement = connection.prepareStatement(
+                "insert into post(title, link, description, created) values (?,?,?,?) on conflict do nothing;")) {
+            statement.setString(1, post.getTitle());
+            statement.setString(2, post.getLink());
+            statement.setString(3, post.getDescription());
+            statement.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> result = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(
+                "select * from post;"
+        )) {
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                result.add(new Post(
+                        rs.getInt("id"),
+                        rs.getString("title"),
+                        rs.getString("link"),
+                        rs.getString("description"),
+                        rs.getTimestamp("created").toLocalDateTime()
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post result = null;
+        try (PreparedStatement statement = connection.prepareStatement(
+                "select * from post where id = ?;"
+        )) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                result = new Post(
+                        resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getString("link"),
+                        resultSet.getString("description"),
+                        resultSet.getTimestamp("created").toLocalDateTime()
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
